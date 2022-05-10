545cf3ee2a064c929781db0c6cda75925ca675e2
javilamarque <jvrlamarque@gmail.com>
2022-05-10 20:51:32 -0300

    correccion de pagina

---
+++ js/login.js
@@ -0,0 +1,7 @@
+window.onload = function(){
+    const createAccountButton = document.getElementById('submit')
+
+    createAccountButton.addEventListener('click', ()=>{
+        createAccountButton.addEventListener('d-none')
+    })
+}
